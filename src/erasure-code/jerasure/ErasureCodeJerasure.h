b1c553d8ae63cf7cc15e75d42c4271cd33a24c7f
Jamie Pryde <jamiepry@uk.ibm.com>
2024-11-05 22:21:05 +0000

    erasure-code: Add parity delta write optimization to Jerasure
    This commit adds FLAG_EC_PLUGIN_PARITY_DELTA_OPTIMIZATION to the list of
    optimizations supported by the Jerasure plugin, and encode_delta and
    apply_delta functions for all Jerasure techniques.
    
    Signed-off-by: Jamie Pryde <jamiepry@uk.ibm.com>

--- src/erasure-code/jerasure/ErasureCodeJerasure.h
+++ src/erasure-code/jerasure/ErasureCodeJerasure.h
@@ -49,7 +49,8 @@ public:
   uint64_t get_supported_optimizations() const override {
     return FLAG_EC_PLUGIN_PARTIAL_READ_OPTIMIZATION |
       FLAG_EC_PLUGIN_PARTIAL_WRITE_OPTIMIZATION |
-      FLAG_EC_PLUGIN_ZERO_INPUT_ZERO_OUTPUT_OPTIMIZATION;
+      FLAG_EC_PLUGIN_ZERO_INPUT_ZERO_OUTPUT_OPTIMIZATION |
+      FLAG_EC_PLUGIN_PARITY_DELTA_OPTIMIZATION;
   }
 
   unsigned int get_chunk_count() const override {
@@ -74,10 +75,10 @@ public:
 
   void encode_delta(const ceph::bufferptr &old_data,
                     const ceph::bufferptr &new_data,
-                    ceph::bufferptr *delta);
+                    ceph::bufferptr *delta) override;
 
-  void apply_delta(const std::map<int, ceph::bufferptr> &in,
-                   std::map <int, ceph::bufferptr> &out);
+  virtual void apply_delta(const std::map<int, ceph::bufferptr> &in,
+                           std::map <int, ceph::bufferptr> &out) = 0;
 
   int init(ceph::ErasureCodeProfile &profile, std::ostream *ss) override;
 
@@ -91,6 +92,18 @@ public:
   virtual unsigned get_alignment() const = 0;
   virtual void prepare() = 0;
   static bool is_prime(int value);
+
+  void matrix_apply_delta(const std::map<int, bufferptr> &in,
+                          std::map <int, bufferptr> &out,
+                          int k, int w, int *matrix);
+
+  void schedule_apply_delta(const std::map<int, bufferptr> &in,
+                            std::map <int, bufferptr> &out,
+                            int k, int w, int packetsize,
+                            int ** simple_schedule);
+
+  void do_scheduled_ops(char **ptrs, int **operations, int packetsize, int s, int d);
+  
 protected:
   virtual int parse(ceph::ErasureCodeProfile &profile, std::ostream *ss);
 };
@@ -118,6 +131,8 @@ public:
                                char **data,
                                char **coding,
                                int blocksize) override;
+  void apply_delta(const std::map<int, ceph::bufferptr> &in,
+                   std::map <int, ceph::bufferptr> &out) override;
   unsigned get_alignment() const override;
   void prepare() override;
 private:
@@ -148,6 +163,8 @@ public:
                                char **data,
                                char **coding,
                                int blocksize) override;
+  void apply_delta(const std::map<int, ceph::bufferptr> &in,
+                   std::map <int, ceph::bufferptr> &out) override;
   unsigned get_alignment() const override;
   void prepare() override;
 private:
@@ -160,12 +177,14 @@ class ErasureCodeJerasureCauchy : public ErasureCodeJerasure {
 public:
   int *bitmatrix;
   int **schedule;
+  int **simple_schedule;
   int packetsize;
 
   explicit ErasureCodeJerasureCauchy(const char *technique) :
     ErasureCodeJerasure(technique),
     bitmatrix(0),
     schedule(0),
+    simple_schedule(0),
     packetsize(0)
   {
     DEFAULT_K = "7";
@@ -181,6 +200,8 @@ public:
                                char **data,
                                char **coding,
                                int blocksize) override;
+  void apply_delta(const std::map<int, ceph::bufferptr> &in,
+                   std::map <int, ceph::bufferptr> &out) override;
   unsigned get_alignment() const override;
   void prepare_schedule(int *matrix);
 private:
@@ -209,12 +230,14 @@ class ErasureCodeJerasureLiberation : public ErasureCodeJerasure {
 public:
   int *bitmatrix;
   int **schedule;
+  int **simple_schedule;
   int packetsize;
 
   explicit ErasureCodeJerasureLiberation(const char *technique = "liberation") :
     ErasureCodeJerasure(technique),
     bitmatrix(0),
     schedule(0),
+    simple_schedule(0),
     packetsize(0)
   {
     DEFAULT_K = "2";
@@ -230,6 +253,8 @@ public:
                                char **data,
                                char **coding,
                                int blocksize) override;
+  void apply_delta(const std::map<int, ceph::bufferptr> &in,
+                   std::map <int, ceph::bufferptr> &out) override;
   unsigned get_alignment() const override;
   virtual bool check_k(std::ostream *ss) const;
   virtual bool check_w(std::ostream *ss) const;
